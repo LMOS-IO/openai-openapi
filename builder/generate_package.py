49de1caf70cd0fb726f8ab194024bfa9e938d8b0
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-10-13 02:55:13 +0000

    setup automatic builds

---
+++ builder/generate_package.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from datamodel_code_generator import InputFileType, generate
+from datamodel_code_generator import DataModelType, PythonVersion
+import json
+import warnings
+
+from ruamel.yaml import YAML
+from ruamel.yaml.error import ReusedAnchorWarning
+
+# fix errors from ruamel.yaml
+warnings.simplefilter("ignore", ReusedAnchorWarning)
+
+# convert the yaml into json
+yaml = YAML(typ="safe", pure=True)
+
+with open("openapi.yaml") as stream:
+    data = yaml.load(stream)
+
+openapi_json = json.dumps(data)
+
+# create a path for the new package
+release_pkg_dir = Path('openai_types/')
+release_pkg_dir.mkdir(parents=True, exist_ok=True)
+
+# create types
+output = Path(release_pkg_dir / '__init__.py')
+generate(
+    input_=openapi_json,
+    input_file_type=InputFileType.OpenAPI,
+    output=output,
+    output_model_type=DataModelType.PydanticV2BaseModel,
+    target_python_version=PythonVersion.PY_310,
+    field_constraints=True,   
+)
