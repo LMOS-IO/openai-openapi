03b3b965c91f14b04c5de8880c5d823cf931394f
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-10-20 21:32:44 +0000

    add regex to fix '...' statements

--- builder/generate_package.py
+++ builder/generate_package.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import re
+from tempfile import TemporaryDirectory
 from datamodel_code_generator import InputFileType, generate, OpenAPIScope
 from datamodel_code_generator import DataModelType, PythonVersion
 import json
@@ -23,22 +25,37 @@ release_pkg_dir = Path('openai_types/')
 release_pkg_dir.mkdir(parents=True, exist_ok=True)
 
 # create types
-output = Path(release_pkg_dir / '__init__.py')
-generate(
-    input_=openapi_json,
-    input_file_type=InputFileType.OpenAPI,
-    output=output,
-    output_model_type=DataModelType.PydanticV2BaseModel,
-    target_python_version=PythonVersion.PY_312,
-    apply_default_values_for_required_fields=False,
-    use_field_description=True,
-    use_schema_description=True,
-    use_subclass_enum=True,
-    enum_field_as_literal="all",
-    # reuse_model=True,
-    # use_default_kwarg=True,
-    # field_constraints=True,
-    # openapi_scopes=[OpenAPIScope.Parameters, OpenAPIScope.Paths, OpenAPIScope.Schemas, OpenAPIScope.Tags],
-    strict_nullable=True # https://github.com/koxudaxi/datamodel-code-generator/issues/327
-)
 
+def gen(path):
+    generate(
+        input_=openapi_json,
+        input_file_type=InputFileType.OpenAPI,
+        output=path,
+        output_model_type=DataModelType.PydanticV2BaseModel,
+        target_python_version=PythonVersion.PY_312,
+        apply_default_values_for_required_fields=False,
+        use_field_description=True,
+        use_schema_description=True,
+        use_subclass_enum=True,
+        enum_field_as_literal="all",
+        # reuse_model=True,
+        # use_default_kwarg=True,
+        field_constraints=True,
+        # openapi_scopes=[OpenAPIScope.Parameters, OpenAPIScope.Paths, OpenAPIScope.Schemas, OpenAPIScope.Tags],
+        strict_nullable=True # https://github.com/koxudaxi/datamodel-code-generator/issues/327
+    )
+
+with TemporaryDirectory() as temporary_directory:
+    tmp_dir_path = Path(temporary_directory)
+    output = Path(tmp_dir_path / 'model.py')
+    gen(output)
+    model: str = output.read_text()
+
+pattern = r'(.*?: Optional\[.*\] = Field\(\n?)\.\.\.'
+replace = r'\1None'
+
+model = re.sub(pattern, replace, model)
+
+output = Path(release_pkg_dir / '__init__.py')
+with open(output, "r+") as file:
+    file.write(model)
